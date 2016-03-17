2e39eb684f6de58104c9dc9870a97dcdeb832865
chabbaboy <chabba@gmail.com>
2016-03-17 10:28:47 +0100

    brisanje soficstivirane procedure

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var configRoutes = require('./routes/config');
+var instancesRoutes = require('./routes/instances');
+var modelRoutes = require('./routes/model');
 
 var app = express();
 
 app.use('/api', configRoutes());
+app.use('/api', instancesRoutes());
+app.use('/api', modelRoutes());
 
 app.listen(3000);
